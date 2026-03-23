937e826007d3c39b4157652a940163965a4e3160
abdouhenni1230-ctrl <abdouhenni1230@gmail.com>
2026-03-23 23:39:57 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,36 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.8773690226:AAEWCxX0Yjo1fat4Bpzsc9-8vAjXoHCZTu8;
+
+const bot = new TelegramBot(token,{polling:true});
+
+bot.onText(/\/start/,msg=>{
+    
+    bot.sendMessage(msg.chat.id,"مرحبا 👋\nاضغط لشراء كود مقابل 5 نجوم ⭐",{
+        reply_markup:{
+            inline_keyboard:[
+                [{text:"شراء كود ⭐",callback_data:"buy"}]
+            ]
+        }
+    });
+
+});
+
+bot.on("callback_query",query=>{
+    
+    if(query.data==="buy"){
+        
+        const prices=[{label:"code",amount:5}];
+
+        bot.sendInvoice(
+            query.message.chat.id,
+            "Buy Code",
+            "Get access code",
+            "payload",
+            "",
+            "XTR",
+            prices
+        );
+    }
+
+});
