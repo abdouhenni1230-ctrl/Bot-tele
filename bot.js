12e34b45fe37d252ed137fb9fe431a337660429b
abdouhenni1230-ctrl <abdouhenni1230@gmail.com>
2026-03-24 00:42:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,37 +1,36 @@
-‎const TelegramBot = require("node-telegram-bot-api");
-‎
-‎const token = process.env.BOT_TOKEN;
-‎
-‎const bot = new TelegramBot(token,{polling:true});
-‎
-‎bot.onText(/\/start/,msg=>{
-‎    
-‎    bot.sendMessage(msg.chat.id,"مرحبا 👋\nاضغط لشراء كود مقابل 5 نجوم ⭐",{
-‎        reply_markup:{
-‎            inline_keyboard:[
-‎                [{text:"شراء كود ⭐",callback_data:"buy"}]
-‎            ]
-‎        }
-‎    });
-‎
-‎});
-‎
-‎bot.on("callback_query",query=>{
-‎    
-‎    if(query.data==="buy"){
-‎        
-‎        const prices=[{label:"code",amount:5}];
-‎
-‎        bot.sendInvoice(
-‎            query.message.chat.id,
-‎            "Buy Code",
-‎            "Get access code",
-‎            "payload",
-‎            "",
-‎            "XTR",
-‎            prices
-‎        );
-‎    }
-‎
-‎});
-‎
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token,{polling:true});
+
+bot.onText(/\/start/,msg=>{
+    
+    bot.sendMessage(msg.chat.id,"مرحبا 👋\nاضغط لشراء كود مقابل 5 نجوم ⭐",{
+        reply_markup:{
+            inline_keyboard:[
+                [{text:"شراء كود ⭐",callback_data:"buy"}]
+            ]
+        }
+    });
+
+});
+
+bot.on("callback_query",query=>{
+    
+    if(query.data==="buy"){
+        
+        const prices=[{label:"code",amount:5}];
+
+        bot.sendInvoice(
+            query.message.chat.id,
+            "Buy Code",
+            "Get access code",
+            "payload",
+            "",
+            "XTR",
+            prices
+        );
+    }
+
+});
